a3d56d8c105d288035b7ed3db719756f74f61030
Rattle189 <48095450+Rattle189@users.noreply.github.com>
2025-11-19 18:57:23 +0800

    Skeleton of PersonnelManager

---
+++ PersonnelManager.java
@@ -0,0 +1,33 @@
+package com.lighthawk.ctwms;
+
+// Necessary imports for the PersonnelManager class
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class PersonnelManager {
+    // Declares a private global variable for the PersonnelManager class itself to use
+    private LinkedList<String> campusPersonnel = new LinkedList<>();
+    
+    // Add main functionality of the personnel manager here!
+    
+    // METHODS BELOW!
+    private void insertPersonnel() {
+        // Do something.
+    }
+    
+    private void removePersonnel() {
+        // Oh no! Someone's getting removed?
+    }
+    
+    private void searchPersonnel() {
+        // Who are you looking for?
+    }
+    
+    private void sortPersonnel() {
+        // Eh. It's supposed to do it alphabetically?
+    }
+    
+    private void countTotalPersonnel() {
+        // I assume we need some kind of basic addition equation here
+    }
+}
